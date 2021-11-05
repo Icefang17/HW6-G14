1f5aaa320dbf6965e8ac820cc3e3eee7a8b838d0
David McCullah <davidwmccullah@gmail.com>
2021-11-05 13:04:58 -0500

    Working

---
+++ src/game/CSP.java
@@ -0,0 +1,14 @@
+package game;
+
+import java.util.ArrayList;
+import resource.*;
+
+public class CSP {
+    ArrayList<Box> variables;
+    ArrayList<ArrayList<Integer>> domains;
+    public  constraints(value) {
+        foreach(Box var : variables) {
+
+        }
+    }
+}
--- src/search/BackTrackingSearch.java
+++ src/search/BackTrackingSearch.java
@@ -3,25 +3,25 @@ package search;
 import java.awt.Point;
 import java.util.ArrayList;
 
-public class BackTrackingSearch {
-    // Assignments have:
-    // A variable to assign
-    // A value to assign the variable to
-    // An array of inferences 
+import resource.*;
 
-    public State backtrackingSearch(Problem csp) { // returns a solution, or failure
-        
-        return backtrack({}, csp); // Will return an array of assignments.
+// What is assignment? inference?
+// Should the map be a 2d array? Groups of constraints?
+// Is the CSP just the start state here?
+// Order Domain values doesn't actually matter here does it?
+
+public class BackTrackingSearch {
+    public ArrayList<Pair> backtrackingSearch(Problem csp) { // returns a solution, or failure
+        return backtrack({}, csp); // Will return an array of assignments?
     }
 
-    public Assignment backtrack(Assignment assignment, Problem csp) { // returns a solution, or failure
+    public ArrayList<Pair> backtrack(ArrayList<Pair> assignment, Problem csp) { // returns a solution, or failure
         if(assignment.isComplete())
             return assignment;
 
-        // Select box to try.
+        // Select the box with the fewest possible values, or with the most available group spaces on tie break.
         int var = selectUnassignedVariable(csp); // MRV w/ Degree Heuristic function 
 
-        // Select 
         for(int value : orderDomainValues(var, assignment, csp)) { // Least Constraining Value function
             if(assignment.isConsistent(value)) {
                 // add {var = value} to assignment;
@@ -46,20 +46,53 @@ public class BackTrackingSearch {
         return null;
     }
 
-    private int selectUnassigedVariable(Problem csp) {
-        // Select the variable from the problem with the fewest possible values
+    // Will select the variable with the smallest domain.
+    // If there is a tie, will choose the variable with the most empty boxes in its parent sets.
+    private Box selectUnassigedVariable(Problem csp) {
+        // Select the variable from the problem with the fewest possible values ie smallest domain
         // Breaks ties by favoring the variable with the most cumulative open spaces in its row, col, and block.
+        ArrayList<Integer> domain = new ArrayList<>();
+
+        foreach(Box var : csp.variables) {
+            ArrayList<Box> row = Box.getParentRow().getChildren();
+            for(int i = 0; i < row.length) {
+                if(!domain.contains(row.get(i).getNumber()))
+                    domain.add(row.get(i).getNumber());
+            }
+
+            ArrayList<Box> column = Box.getParentColumn().getChildren();
+            for(int i = 0; i < row.length) {
+                if(!domain.contains(row.get(i).getNumber()))
+                    domain.add(row.get(i).getNumber());
+            }
+
+            ArrayList<Box> row = Box.getParentBlock().getChildren();
+            for(int i = 0; i < row.length) {
+                if(!domain.contains(row.get(i).getNumber()))
+                    domain.add(row.get(i).getNumber());
+            }
+        }
     }
 
-    private int orderDomainValues(int var, Assignment assignment, Problem csp) {
+    // Order the domain by least constrained value.
+    // This is arbitrary, domain values all share the same constraints.
+    private int orderDomainValues(Box var, ArrayList<Pair> assignment, Problem csp) {
+        return csp.domain;
         // Orders the domain values for the current variable by least constrained value.
         // Basically computes the domain from all 3 directions and orders it by ???.
     }
 
-    private domain? inference(Problem csp, int var, int value) {
+    private domain? inference(Problem csp, Box var, int value) {
         // Establishes arc consistency for the current variable.
         
         // Remove value from surrounding domains. If any domains are empty, return null
         // get boxes from csp at [var], remove value from row, col, block domain.
     }
+}
+
+for(int i = 0; i < 9; i++) {
+    if !rows.get(i).isComplete
+        return false;
+    if !cols.get(i).isComplete  
+        return false;
 }
